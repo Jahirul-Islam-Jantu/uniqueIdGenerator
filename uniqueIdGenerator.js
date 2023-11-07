ad6e08d4012fba99082c244d5c1f0c5b8d3b82ad
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-11-07 11:06:57 +0600

    first Commit

---
+++ uniqueIdGenerator.js
@@ -0,0 +1,9 @@
+function uuidv4(){
+    return 'xxxxxxxx-xxxx-4xxx-xxxxxxxxxxxx'.replace(/[xy]/g,(c) =>{
+        const r = (Math.random()*16) | 0;
+        const v = c == 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    })
+}
+
+console.log(uuidv4());
